c6cc8bcc8e268703222537f3ec72f6e1622dacbe
Arnav Tripathi <arnavtripathi21045@gmail.com>
2026-06-22 01:03:50 +0530

    Fix Postgres SSL connection drop by adding pool_pre_ping

--- database.py
+++ database.py
@@ -16,7 +16,7 @@ if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False, "timeout": 30})
 else:
-    engine = create_engine(DATABASE_URL)
+    engine = create_engine(DATABASE_URL, pool_pre_ping=True, pool_recycle=1800)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
