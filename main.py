5196e50fda562cc9bd0c372e8057a875ec28ceec
Sarvyagya Prakash <prakash.sarvyagya@gmail.com>
2026-07-08 22:09:39 +0530

    chore: update CORS configuration to allow new frontend URL

--- main.py
+++ main.py
@@ -33,7 +33,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://orchestra-frontend-roan.vercel.app",
+        "http://localhost:3000",
+        "http://localhost:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
